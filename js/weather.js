f2af44a618b0c5e21ca1211118c7c38faba02752
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2021-11-01 02:40:25 +0900

    fixed error

--- js/weather.js
+++ js/weather.js
@@ -8,14 +8,14 @@ const API_KEY = "9ffd1581a6aa8e045e626ae0cbf35da2";
 function geoInform(event) {
   const lat = event.coords.latitude;
   const log = event.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       city.innerText = `${data.name}`;
-      temp.innerText = `${data.main.temp}°`;
+      temp.innerText = `${data.main.temp}℃`;
       weather.innerText = `${data.weather[0].main}`;
-      maxTemp.innerText = `Temp max : ${data.main.temp_max}° / Temp min : ${data.main.temp_min}°`;
+      maxTemp.innerText = `Temp max : ${data.main.temp_max}℃ / Temp min : ${data.main.temp_min}℃`;
     });
 }
 
